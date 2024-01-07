d1114dee91189445938ef08b078b2ed3cdd9a003
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-01-07 02:22:43 -0300

    header and nav
    finalizei a estrutura do header e nav, agora vou fazer o main

---
+++ index.js
@@ -0,0 +1,8 @@
+function myNav () {
+    let nav = document.getElementById('navigation');
+    if (nav.style.display === "none") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+}
