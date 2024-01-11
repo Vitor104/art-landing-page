061da7dc3d25a4cbf40d90bb617628218238588d
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-01-11 19:50:54 -0300

    nav finalizado
    nav está finalizado, faltando apenas a estilização

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function myNav () {
-    let nav = document.getElementById('navigation');
+    let nav = document.getElementById('mobile-nav');
+    let navBar = document.getElementById('verNav')
+    let navBarMin = document.getElementById('minimizar')
+
     if (nav.style.display === "none") {
         nav.style.display = "block";
     } else {
         nav.style.display = "none";
     }
+
+    
 }
